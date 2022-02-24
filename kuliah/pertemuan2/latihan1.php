d4eb3c5a72ce65cfe28286294f9a50df826522af
maulafiddina <dzikra.213040081@mail.unpas.ac.id>
2022-02-24 17:04:36 +0700

    latihan pertemuan2

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,63 @@
+<?php
+
+// Komentar
+
+// variable
+// integer angka
+// string tulisan
+// boolean true dan false, jika true = 1 false = null(tidak ada nilai)
+// nama bebas tidak boleh ada spasi
+// ada angka tapi tidak diawal
+$nama = "Dzikra maula fiddina";
+echo $nama;
+
+echo "<hr>";
+
+
+// oprator
+// oprator aritmatika
+// +, -, *, /, %
+//  % modulus (sisa pembagian)
+
+echo (1 + 2) * 3 - 4;
+echo "<br>";
+
+echo 10 % 5;
+
+echo "<hr>";
+
+// oprator perbandingan
+// <, >, =,
+
+echo 2 < 4;
+
+echo "<br>";
+
+// indentity
+// ===, /==
+// untuk mengecek nilai serta tipe data
+
+echo 10 === 10;
+
+echo "<hr>";
+
+// interment dan determent
+// penambahan dan pengurangan 1
+// ++, --
+
+$X = 10;
+$X++;
+echo $X;
+echo "<hr>";
+
+$X = 10;
+$X--;
+echo $X;
+echo "<hr>";
+
+// concat, penggabungan string ; .
+$nama_depan = "Dzikra" ;
+$nama_belakang = "Maula" ;
+echo $nama_depan . " " . $nama_belakang ;
+
+?>
