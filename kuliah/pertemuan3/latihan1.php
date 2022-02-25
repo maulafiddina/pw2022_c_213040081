0c7cffd45cd05131e1d6f228d1fd3e5ad9d11b93
maulafiddina <dzikra.213040081@mail.unpas.ac.id>
2022-02-25 17:18:59 +0700

    Latihan pertemuan 3

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,24 @@
+<?php
+// Pengulangan
+// for
+// while
+// do.. while
+// foreach : pengulanga khusus array
+
+for( $i = 0; $i < 5; $i++ ) {
+    echo "Hello World <br>";
+}
+
+$i = 0;
+while( $i < 5 ) {
+    echo "hello world! <br>";
+$i++;
+}
+
+$i = 0;
+do {
+    echo "hello world! <br>";
+$i++;
+} while( $i < 5 );
+
+?>
