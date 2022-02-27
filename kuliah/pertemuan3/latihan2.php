870dbea59dca0d9c6798f23d5f6e71c6c1132af4
maulafiddina <dzikra.213040081@mail.unpas.ac.id>
2022-02-27 17:08:20 +0700

    latihan pertemuan 3

---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,16 @@
+<?php
+// Pengkodisian / percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 10;
+if( $x < 20 ) {
+    echo "Yes";
+} elseif( $x = 20) {
+    echo "Bang!";
+} else {
+    echo "No";
+}
+?>
